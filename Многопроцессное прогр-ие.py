61d9290ca2db82b168d3ca8b5622e7c6a4c44884
SergeiMartunyuk <alessaralfinit@gmail.com>
2025-01-15 17:56:30 +0500

    Update Многопроцессное прогр-ие.py

--- Многопроцессное прогр-ие.py
+++ Многопроцессное прогр-ие.py
@@ -6,20 +6,19 @@ def read_info(name):
     with open(name, 'r', encoding='utf-8') as file:
         for line in file:
             all_data.append(line)
-    return all_data
-
-filenames = [f'./file {number}.txt' for number in range(1, 5)]
-start = time.time()
-over = time.time()
-time_1 = over - start
-start_2 = time.time()
-print(f'Линейный вызов: {time_1}')
+    #return all_data
 
 if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    start = time.time()
+    over = time.time()
+    time_1 = over - start
+    start_2 = time.time()
+    print(f'Линейный вызов: {time_1}')
 
     with Pool(processes=4) as pool:
         pool.map(read_info, filenames)
-over_2 = time.time()
-time_2 = over_2 - start_2
-print(f'Многопроцессный вызов: {time_2}')
-
+    over_2 = time.time()
+    time_2 = over_2 - start_2
+    print(f'Многопроцессный вызов: {time_2}')
